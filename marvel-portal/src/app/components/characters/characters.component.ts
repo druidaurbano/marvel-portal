70b86d3b7897e0d74df87041d58162465d27dcf0
lucascorrea22 <lucas.delta22@gmail.com>
2023-09-28 22:46:15 -0300

    tela de criadores e personagens com dados vindos da api

--- marvel-portal/src/app/components/characters/characters.component.ts
+++ marvel-portal/src/app/components/characters/characters.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Character } from 'src/app/models/character.model';
+import { ApiMarvelService } from 'src/app/services/api-marvel.service';
 
 @Component({
   selector: 'app-characters',
@@ -9,8 +10,10 @@ import { Character } from 'src/app/models/character.model';
 export class CharactersComponent {
   charactersList: Array<Character> = [];
 
-  constructor() {
-    this.charactersList = [
+  constructor(
+    private apiMarvel: ApiMarvelService
+  ) {
+    /* this.charactersList = [
       {
         id: 1,
         name: 'Loki',
@@ -41,6 +44,27 @@ export class CharactersComponent {
         name: 'Loki',
         thumbnail: '../../../assets/character.jpg'
       },
-    ]
+    ] */
+  }
+
+  ngOnInit() {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    this.apiMarvel.getCharacters().subscribe((data: any) => {
+      console.log('show me the data inside comics component', data.data?.results);
+      for(let item of data.data?.results) {
+        let character: Character = {
+          id: item.id,
+          name: item.name,
+          thumbnail: `${item.thumbnail.path}.${item.thumbnail.extension}`
+        };
+
+        console.log('show me the character', character);
+        this.charactersList.push(character);
+      }
+      //console.log('show me the posts', this.posts);
+    });
   }
 }
--- marvel-portal/src/app/components/creators/creators.component.ts
+++ marvel-portal/src/app/components/creators/creators.component.ts
@@ -1,3 +1,4 @@
+import { ApiMarvelService } from 'src/app/services/api-marvel.service';
 import { Component } from '@angular/core';
 import { Creator } from 'src/app/models/creator.model';
 
@@ -9,8 +10,10 @@ import { Creator } from 'src/app/models/creator.model';
 export class CreatorsComponent {
   creatorsList: Array<Creator> = [];
 
-  constructor() {
-    this.creatorsList = [
+  constructor(
+    private apiMarvel: ApiMarvelService
+  ) {
+    /* this.creatorsList = [
       {
         id: 1,
         fullName: 'Johnson Gallak',
@@ -41,6 +44,25 @@ export class CreatorsComponent {
         fullName: 'Johnson Gallak',
         thumbnail: '../../../assets/creator.jpg'
       },
-    ]
+    ] */
+  }
+
+  ngOnInit() {
+    this.getCreators();
+  }
+
+  getCreators() {
+    this.apiMarvel.getCreators().subscribe((data: any) => {
+      console.log('show me the data inside events', data.data?.results);
+      for(let item of data.data?.results) {
+        let creator: Creator = {
+          id: item.id,
+          fullName: item.fullName,
+          thumbnail: `${item.thumbnail.path}.${item.thumbnail.extension}`
+        }
+        console.log('show me the creator', creator);
+        this.creatorsList.push(creator);
+      }
+    });
   }
 }
